ae6d026501911736149e2ae7c45a95ecc45f3184
k1LoW <k1lowxb@gmail.com>
2014-02-26 22:39:47 +0900

    Initial Commit

---
+++ 101000LAB_MultiQueue/_01000LAB_MultiQueuePlugIn.h
@@ -0,0 +1,17 @@
+//
+//  _01000LAB_MultiQueuePlugIn.h
+//  101000LAB_MultiQueue
+//
+//  Created by k1LoW on 2014/02/26.
+//  Copyright (c) 2014年 101000LAB. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface _01000LAB_MultiQueuePlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
